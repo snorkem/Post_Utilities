7fb09b5c942d84d95e8ba51b14c005cdeb54aa0c
snorkem <snorkem@gmail.com>
2024-12-15 23:15:22 -0800

    Adding a script to add media stats to other columns of the spreadsheet.

--- add_media_stats_to_excel.py
+++ add_media_stats_to_excel.py
@@ -6,6 +6,7 @@ import subprocess
 import json
 from timecode import Timecode
 
+# Define spreadsheet column names
 FILES_TO_GET_STATS = 'Master Original Name'
 FILE_SIZE_COL = 'Master File Size (GB)'
 DIMENSIONS_COL = 'Master Resolution'
@@ -14,7 +15,7 @@ FRAME_RATE_COL ='Master FPS'
 DURATION_COL = 'Master Duration'
 START_TC_COL = 'Master Start'
 
-
+# Instantiate large list of stats to keep track of.
 STATS_TO_UPDATE = {
         'Thumbnail': 'Unknown', 'Name': 'Unknown', 'First File Name': 'Unknown', 'Manufacturer': 'Unknown', 'Size (GiB)': 'Unknown', 'Codec': 'Unknown',
         'Bitrate': 'Unknown', 'Frame Rate': 'Unknown', 'Width': 'Unknown', 'Height': 'Unknown',
@@ -35,10 +36,9 @@ df = df.fillna('')
 file_names = df[FILES_TO_GET_STATS]
 files_to_rename = []
 
-def get_file_size_GiB(target_file: Path):
-    # Returns GiB
+def get_file_size_GB(target_file: Path):
     total_size = target_file.stat().st_size
-    print(total_size)
+    # Returning size the way MacOS reports it as opposed
     return round(total_size / 1000 ** 3, 2)
 
 def get_file_paths(target_dir):
@@ -50,7 +50,7 @@ def get_file_paths(target_dir):
                 files_to_rename.append(file_path)
             elif Path(file_path).is_dir():
                 print('Looking at subdirectory:' + str(file_path))
-                get_file_paths(file_path)
+                get_file_paths(file_path) # recursively get all files into the list of paths
     else:
         print('Target is not a directory')
         quit()
@@ -67,22 +67,26 @@ def convert_duration_tc_(fps, duration_from_json):
     return tc1
 
 def get_file_stats(file: Path, stats_to_update):
+    # Formulate an command using ffprobe terminal arguments and grab the output as a json object.
     cmnd = ['ffprobe', '-print_format', 'json', '-show_format', '-show_streams', '-pretty', '-loglevel', 'quiet', file]
     p = subprocess.Popen(cmnd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = p.communicate()
     if err:
         print("========= error ========")
         print(err)
-    media_stats = json.loads(out)['streams'][0]
-    start_tc = convert_duration_tc_(str(media_stats['r_frame_rate']), str(media_stats['start_time']))
-    stats_to_update.update({
-        'Width': str(media_stats['width']),
-        'Height': str(media_stats['height']),
-        'Codec': str(media_stats['codec_name']),
-        'Frame Rate': str(Timecode(media_stats['r_frame_rate']).framerate),
-        'Duration': str(convert_duration_tc_(str(media_stats['r_frame_rate']), str(media_stats['duration']))),
-        'Start Time': str(start_tc)
-    })
+    try:
+        media_stats = json.loads(out)['streams'][0]
+        start_tc = convert_duration_tc_(str(media_stats['r_frame_rate']), str(media_stats['start_time']))
+        stats_to_update.update({
+            'Width': str(media_stats['width']),
+            'Height': str(media_stats['height']),
+            'Codec': str(media_stats['codec_name']),
+            'Frame Rate': str(Timecode(media_stats['r_frame_rate']).framerate),
+            'Duration': str(convert_duration_tc_(str(media_stats['r_frame_rate']), str(media_stats['duration']))),
+            'Start Time': str(start_tc)
+        })
+    except KeyError as e:
+        print(f'Error getting stream info from file, also, this: {e}')
     return stats_to_update
 
 # Populate dataframe rows
@@ -90,25 +94,23 @@ def update_df_stats(df_in: pd.DataFrame, file_col: str):
     target_files = get_file_paths(target_dir)
     for row, value in df_in.iterrows():
         file_name_in_list = value[file_col]
-        # print(target_files)
-        # print(f'We\'re on row {row}')
-        # print(file_name_in_list)
         for file in target_files:
             file_name = os.path.splitext(os.path.basename(file))[0]
             file_ext = os.path.splitext(os.path.basename(file))[1]
             if file_name == file_name_in_list:
                 print(f'Looking at: {file_name}')
                 print(f'{file} found! Matches name in spreadsheet.')
-                new_file_stats = get_file_stats(file, STATS_TO_UPDATE)
-                # print(new_file_stats)
-                df_in.at[row, FILE_SIZE_COL] = get_file_size_GiB(file)
-                df_in.at[row, DIMENSIONS_COL] = str(new_file_stats['Width'] + ' x ' + new_file_stats['Height'])
-                df_in.at[row, CODEC_COL] = str(new_file_stats['Codec'])
-                df_in.at[row, FRAME_RATE_COL] = str(new_file_stats['Frame Rate'])
-                df_in.at[row, DURATION_COL] = str(new_file_stats['Duration'])
-                df_in.at[row, START_TC_COL] = str(new_file_stats['Start Time'])
-               # print(f'{file} resolution is {df.at[row, FILE_SIZE_COL]}')
-               # print(f'start tc is {df.at[row, START_TC_COL]}')
+                try:
+                    new_file_stats = get_file_stats(file, STATS_TO_UPDATE)
+                    # print(new_file_stats)
+                    df_in.at[row, FILE_SIZE_COL] = get_file_size_GB(file)
+                    df_in.at[row, DIMENSIONS_COL] = str(new_file_stats['Width'] + ' x ' + new_file_stats['Height'])
+                    df_in.at[row, CODEC_COL] = str(new_file_stats['Codec'])
+                    df_in.at[row, FRAME_RATE_COL] = str(new_file_stats['Frame Rate'])
+                    df_in.at[row, DURATION_COL] = str(new_file_stats['Duration'])
+                    df_in.at[row, START_TC_COL] = str(new_file_stats['Start Time'])
+                except KeyError as e:
+                    print(f'Error getting stream info from file, also, this: {e}')
     return df
 
 def main():
